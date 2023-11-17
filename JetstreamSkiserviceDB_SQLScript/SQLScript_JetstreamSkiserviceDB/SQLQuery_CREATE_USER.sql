e42cd324c3625d1dee3711766af43a596ba8463a
mahgoe <gonenmahir@hotmail.com>
2023-11-17 11:20:52 +0100

    change sql server user

--- JetstreamSkiserviceDB_SQLScript/SQLScript_JetstreamSkiserviceDB/SQLQuery_CREATE_USER.sql
+++ JetstreamSkiserviceDB_SQLScript/SQLScript_JetstreamSkiserviceDB/SQLQuery_CREATE_USER.sql
@@ -1,37 +1,18 @@
 /* 
 	This Query creates a admin login for the JetstreamSkiservice database.
 */
-
--- Change master to create user
 USE master;
 GO
 
--- Give the login a role
-ALTER SERVER ROLE sysadmin ADD MEMBER JetstreamSkiservice_admin;
+CREATE LOGIN [Meier] WITH PASSWORD = 'password';
 GO
 
--- Create new login with password
-CREATE LOGIN JetstreamSkiservice_admin WITH PASSWORD = 'password';
+USE [JetstreamSkiserviceDB];
 GO
 
--- Change to the specific database
-USE JetstreamSkiserviceDB;
+CREATE USER [Meier] FOR LOGIN [Meier];
 GO
 
--- Create a new user with the login
-CREATE USER JetstreamSkiservice_admin FOR LOGIN JetstreamSkiservice_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE TO [Meier];
 GO
 
--- Add user to the role
-ALTER ROLE db_owner ADD MEMBER JetstreamSkiservice_admin;
-GO
-
--- Give permissions to the role
-GRANT INSERT, SELECT, UPDATE, DELETE ON OBJECT::dbo.Employees TO DB_employee;
-GRANT INSERT, SELECT, UPDATE, DELETE ON OBJECT::dbo.Priority TO DB_employee;
-GRANT INSERT, SELECT, UPDATE, DELETE ON OBJECT::dbo.Registrations TO DB_employee;
-GRANT INSERT, SELECT, UPDATE, DELETE ON OBJECT::dbo.Services TO DB_employee;
-GRANT INSERT, SELECT, UPDATE, DELETE ON OBJECT::dbo.Status TO DB_employee;
-
--- Assigning the user to the role
-EXEC sp_addrolemember 'DB_employee', 'JetstreamSkiservice_admin';
