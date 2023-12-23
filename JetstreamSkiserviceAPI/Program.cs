92d64dc873dcadee83275c07dc0e1e20416d0513
mahgoe <139371012+mahgoe@users.noreply.github.com>
2023-12-23 02:01:23 +0100

    init swagger authorization

--- JetstreamSkiserviceAPI/Program.cs
+++ JetstreamSkiserviceAPI/Program.cs
@@ -67,11 +67,35 @@ namespace JetstreamSkiserviceAPI
                 });
             });
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Jetstream Web API", Version = "v1" });
+                builder.Services.AddEndpointsApiExplorer();
+
+                builder.Services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Jetstream Web API", Version = "v1" });
+                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT Authorization header using the Bearer scheme."
+                    });
+                    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                    new OpenApiSecurityScheme
+                    {
+                    Reference = new OpenApiReference
+                    {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                    }
+                },
+                new string[] {}
+                }
             });
+        });
 
             builder.Services.AddAuthorization();
             builder.Services.AddControllers();
